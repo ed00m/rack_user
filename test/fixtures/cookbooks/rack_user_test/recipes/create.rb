38a8185b61e7a94fdd2c6661e20181bf8eaa4073
Martin Smith <martin@mbs3.org>
2015-05-14 11:48:42 -0500

    Switch to directly downloading the SSH key file
    - Add fetch_url helper for downloading authorized_keys files
    - Remove remote_file resource, as discussed in #1, to make this provider more idempotent
    - ruby_block shenanigans were also no longer needed

--- test/fixtures/cookbooks/rack_user_test/recipes/create.rb
+++ test/fixtures/cookbooks/rack_user_test/recipes/create.rb
@@ -1,6 +1,6 @@
 # comments!
 
 rack_user 'default' do
-  location 'https://gist.githubusercontent.com/jujugrrr/af613872c3d029a94c88/raw/1d57582215ac6316f55b956691db1dba8339fcdd/test'
+  location 'https://gist.githubusercontent.com/martinb3/14595e35648f5a822073/raw/133f3a3cf0bd96f4008fb16a7d4dc2322f4f2a5a/test-authorized-keys'
   action :create
 end
