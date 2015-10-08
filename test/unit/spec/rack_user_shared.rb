967d31eb3cae98b8aa2872d8d3026f7950f8b007
Sia Gholami <myself@seeafish.me>
2015-10-08 13:10:47 +0100

    Defining new parameter for managing passwords from outside source

--- test/unit/spec/rack_user_shared.rb
+++ test/unit/spec/rack_user_shared.rb
@@ -1,7 +1,7 @@
 shared_examples_for 'create the rack user' do
   it 'creates a user rack with the correct keys' do
     # does not step into provider, so ssh_keys is nil
-    expect(chef_run).to create_rack_user('default').with(ssh_keys: nil)
+    expect(chef_run).to create_rack_user('default').with(ssh_keys: nil, passwd_enable: false)
     expect(chef_run).to install_sudo('rack').with(user: 'rack', nopasswd: true)
   end
 end
